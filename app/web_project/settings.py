cfc29fc7bb0b989de6a1f479a64841486e89d5f9
RealLundii <amineboubrit@protonmail.com>
2023-02-15 22:41:16 -0500

    Added home and path

--- app/web_project/settings.py
+++ app/web_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)fq39vpm^s^bt4gi&oyk%w%&k0f+yb@folckf0d$o6r$v1^4!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
